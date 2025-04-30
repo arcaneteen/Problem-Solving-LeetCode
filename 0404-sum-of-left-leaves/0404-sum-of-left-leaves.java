6baa52589e8edb336967958d9a01212ebd5ef36e
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-04-30 19:44:15 +0530

    Time: 0 ms (100%), Space: 42 MB (9.36%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -1,17 +1,16 @@
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-        return dfs(root, false);
+        return dfs(root,false);
     }
-    
     private int dfs(TreeNode node, boolean isLeft) {
         if (node == null) {
             return 0;
         }
-        if (node.left == null && node.right == null) { // Leaf node
+        if (node.left == null && node.right == null) { 
             return isLeft ? node.val : 0;
         }
-        int leftSum = dfs(node.left, true);   // Traverse left child
-        int rightSum = dfs(node.right, false); // Traverse right child
+        int leftSum = dfs(node.left, true);  
+        int rightSum = dfs(node.right, false);
         return leftSum + rightSum;
     }
 }
