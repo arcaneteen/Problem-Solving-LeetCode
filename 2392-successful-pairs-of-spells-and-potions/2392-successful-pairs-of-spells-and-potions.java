0552f55d35ddf560602c04f4ebc71c0763dbcff7
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-05-03 16:08:27 +0530

    Time: 47 ms (41.71%), Space: 65.2 MB (42.2%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,28 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        Arrays.sort(potions);  // Sort potions once
-        int[] arr = new int[spells.length];
-
-        for (int i = 0; i < spells.length; i++) {
-            int spell = spells[i];
-
-            // Binary search to find first potion where spell * potion >= success
-            int low = 0, high = potions.length - 1, index = potions.length;
-            while (low <= high) {
-                int mid = low + (high - low) / 2;
-                if ((long)spell * potions[mid] >= success) {
-                    index = mid;
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
+        int arr[]=new int[spells.length];
+        Arrays.sort(potions);
+        for(int i=0;i<spells.length;i++){
+            int l=0,r=potions.length-1,index=potions.length;
+            while(l<=r){
+                int mid=l+(r-l)/2;
+                if((long)spells[i]*potions[mid]>=success){
+                    index=mid;
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
                 }
             }
-
-            arr[i] = potions.length - index;
+            arr[i]=potions.length-index;
         }
-
         return arr;
     }
 }
