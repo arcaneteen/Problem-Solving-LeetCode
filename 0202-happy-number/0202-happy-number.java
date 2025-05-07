fd0de7b1c87bddebd4286262bc962de5384ea76c
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-05-07 23:38:47 +0530

    Time: 0 ms (100%), Space: 40.5 MB (92.79%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n!=1&& n!=4){
+            int num=0;
+            for(int i=n;i>0;i=i/10){
+                int r=i%10;
+                num+=Math.pow(r,2);
+            }
+            n=num;
+        }
+        if(n==1)  return true;
+        else  return false;
+    }
+}
