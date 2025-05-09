091f283834843431b4676d50c553ff5d2a02ac09
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-05-10 00:31:13 +0530

    Time: 0 ms (100%), Space: 41.9 MB (19.16%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.java
@@ -8,11 +8,11 @@ class Solution {
         for(int s:sandwiches){
             if(s==0){
                 if(zeroS==0)  return oneS; //if now no one wants 0 all 1 will be hungry
-                zeroS--;//if want the sandwich ,decrease 0 number sandwich count by 1;
+                zeroS--;//if want the sandwich ,decrease that person;
             }
             else{
                 if(oneS==0) return zeroS;//if no 1 type of sandwich demand all 0 demanding student will be hungry
-                oneS--;//if demanded decrease stock by 1;
+                oneS--;//if demanded decrease count by 1 person;
             }
         }
         return 0;
