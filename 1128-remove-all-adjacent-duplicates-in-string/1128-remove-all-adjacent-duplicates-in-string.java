1e00c595683981449683a0220479a2f13151a4f4
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-05-12 01:21:51 +0530

    Time: 37 ms (40.66%), Space: 45.8 MB (32.3%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(!stack.isEmpty()&& c==stack.peek()){
+                stack.pop();
+            }
+            else{
+                stack.push(c);
+            }   
+        }
+        StringBuilder str=new StringBuilder();
+        while(!stack.isEmpty()){
+            str.append(stack.pop());
+        }
+        return str.reverse().toString();
+    }
+}
