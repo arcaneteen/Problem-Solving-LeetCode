4c4f2fd112e0ce1113684f241c6bf577977f049b
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-05-15 19:06:54 +0530

    Time: 1 ms (99.79%), Space: 42 MB (93.39%) - LeetHub

---
+++ 1666-make-the-string-great/1666-make-the-string-great.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String makeGood(String s) {
+        StringBuilder stack = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            int len = stack.length();
+            if (len > 0 && Math.abs(stack.charAt(len - 1) - c) == 32) {
+                stack.deleteCharAt(len - 1); // remove last char (bad pair)
+            } else {
+                stack.append(c);
+            }
+        }
+        return stack.toString();
+    }
+}
