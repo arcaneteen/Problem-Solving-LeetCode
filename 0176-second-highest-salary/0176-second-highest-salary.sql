ae7f4c291b175dd63892bb02a61821136217323d
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-05-17 03:33:39 +0530

    Time: 279 ms (63.68%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,3 +2,12 @@
 
 select max(salary) as SecondHighestSalary from Employee where salary<(select max(salary) from Employee)
 #select max(e1.Salary) as SecondHighestSalary from Employee e1 join Employee e2 where e1.Salary<e2.Salary;
+-- SELECT id, salary
+-- FROM Employee
+-- WHERE salary = (
+--     SELECT MAX(salary)
+--     FROM Employee
+--     WHERE salary < (
+--         SELECT MAX(salary) FROM employee
+--     )
+-- );
