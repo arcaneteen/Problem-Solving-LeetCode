40cd921f3f36c80b8981a3e956dc2ac6e7f82381
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-05-17 23:52:56 +0530

    Time: 0 ms (100%), Space: 42.4 MB (53.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long N = n; // Convert to long to avoid overflow when n = Integer.MIN_VALUE
+
+        if (N < 0) {
+            x = 1 / x;  // Invert x for negative powers
+            N = -N;
+        }
+
+        double result = 1;
+        while (N > 0) {
+            if ((N % 2) == 1) result *= x; // Multiply result when bit is 1
+            x *= x;    // Square the base
+            N /= 2;    // Divide exponent by 2
+        }
+
+        return result;
     }
 }
