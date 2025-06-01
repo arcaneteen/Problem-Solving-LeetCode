1b16e937777841f7bda4b6756c988fbd99a687f2
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-06-01 18:03:52 +0530

    Time: 1 ms (99.63%), Space: 44 MB (80.05%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,17 +1,17 @@
 class Solution {
     public int compress(char[] chars) {
-        int index = 0;
-        int i = 0;
-        while (i < chars.length) {
-            char ch = chars[i];
-            int cnt = 0;
-            while (i < chars.length && chars[i] == ch) {
+        int index=0;
+        int i=0;
+        while(i<chars.length){
+            char ch=chars[i];
+            int cnt=0;
+            while(i<chars.length && chars[i]==ch){
                 cnt++;
                 i++;
             }
-            chars[index++] = ch;
-            if (cnt > 1) {
-                for (char c : Integer.toString(cnt).toCharArray()) {
+            chars[index++]=ch;//index 0 p "a" then index increase hoke 1 ab waha count
+            if (cnt>1) {
+                for (char c : String.valueOf(cnt).toCharArray()) {
                     chars[index++] = c;
                 }
             }
