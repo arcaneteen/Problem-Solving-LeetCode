6b6c719f0a32e608ec8025552edf91ec62eac787
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-06-20 15:12:35 +0530

    Time: 0 ms (100%), Space: 40.1 MB (90.55%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,6 +1,13 @@
 class Solution {
     public int fib(int n) {
-        if(n==0||n==1) return n;
-        return fib(n-1)+fib(n-2);
+        if(n==0) return 0;
+        int a=0,b=1;
+        while(n>=1){
+            int c=a+b;
+            a=b;
+            b=c;
+            n--;
+        }
+        return a;
     }
 }
