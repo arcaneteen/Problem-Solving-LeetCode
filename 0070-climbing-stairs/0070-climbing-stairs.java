1057270b697b7971c0ba1c18864869f13151c1b0
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-06-22 15:12:07 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-       if(n<=2) return n;
-       int a=1,b=2;
-       for(int i=3;i<=n;i++){
-        int c=a+b;
-        a=b;
-        b=c;
-       }
-       return b;
+        //no. of ways you can climb stairs
+        if(n<=1) return 1;
+        int[] stair=new int[n+1];
+        stair[0]=stair[1]=1;
+        for(int i=2;i<=n;i++){
+            stair[i]=stair[i-1]+stair[i-2];
+        }
+        return stair[n];
     }
 }
