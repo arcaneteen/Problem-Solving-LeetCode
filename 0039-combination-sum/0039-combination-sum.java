7e744ba2fe0488152b77c98199f5438c7adb170d
KHUSHI KUMARI KESHRI <110624419+arcaneteen@users.noreply.github.com>
2025-07-17 13:15:33 +0530

    Time: 2 ms (82.99%), Space: 44.6 MB (64.24%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(candidates, 0, target, new ArrayList<>(), result);
-        return result;
+        List<List<Integer>> comb=new ArrayList<>();
+        combination(candidates,0,comb,new ArrayList<>(),target);
+        return comb;
     }
-
-    private void backtrack(int[] candidates, int index, int target, List<Integer> current, List<List<Integer>> result) {
-        if (target == 0) {
-            result.add(new ArrayList<>(current)); 
+    void combination(int[] nums,int i,List<List<Integer>> comb,List<Integer> ans,int target){
+        if(target==0){
+            comb.add(new ArrayList<>(ans));
             return;
         }
+        if(i==nums.length || target<0) return;
+        ans.add(nums[i]);
+        // combination(nums,i+1,comb,ans,target-nums[i]);
+        combination(nums,i,comb,ans,target-nums[i]);
+        ans.remove(ans.size()-1);//giving last element index and removing it;
+        combination(nums,i+1,comb,ans,target);
 
-        if (index == candidates.length || target < 0) {
-            return; 
-        }
-
-        
-        if (candidates[index] <= target) {
-            current.add(candidates[index]);
-            backtrack(candidates, index, target - candidates[index], current, result); // stay at same index
-            current.remove(current.size() - 1); 
-        }
-
-    
-        backtrack(candidates, index + 1, target, current, result);
     }
 }
